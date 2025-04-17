4e2238f7d46ee1cc98c11566565828fc29243fa8
Ridanm <danyfox1.dm@gmail.com>
2025-04-17 07:34:15 -0300

    Define a custom matcher

---
+++ spec/09_custom_matcher_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'rspec/expectations'
+
+# If you need to test a condition that does not have a built-in matcher,
+# you can create your own.
+# https://rspec.info/features/3-13/rspec-expectations/custom-matchers/define-matcher/
+
+describe 'defining custom matchers' do
+  context 'when reusing a matcher that is in scope' do
+    matcher :be_divisible_by_four do
+      match { |num| (num % 4).zero? }
+    end
+
+    it 'is divisible by 4' do
+      expect(12).to be_divisible_by_four
+    end
+
+    # You can test for the inverse of the matcher.
+    it 'is not divisible by 4' do
+      expect(99).not_to be_divisible_by_four
+    end
+
+    # You can even use a custom matcher with 'all'.
+    it 'works with multiple values' do
+      expect([12, 100, 800]).to all(be_divisible_by_four)
+    end
+  end
+end
+
+# ASSIGNMENT
+
+describe 'one word palindrome test' do
+  let(:racecar) { 'racecar' }
+  let(:spaceship) { 'spaceship' }
+  let(:rotator) { 'rotator' }
+  let(:palindrome) { 'palindrome' }
+
+  # Write a custom matcher that detects a one word palindrome,
+  # using the following block: { |word| word.reverse == word }
+  # When it is set up correctly, all of the following tests will pass.
+  RSpec::Matchers.define :be_a_palindrome do
+    match { |word| word.reverse == word}
+
+    failure_message do |actual|
+      "expected #{actual} to ba a palindrome" 
+    end
+
+    failure_message_when_negated do |actual|
+      "expected #{actual} not to be a palindrome"
+    end
+  end
+
+  context 'when a palindrome is used' do
+    # remove the 'x' before running this test
+    it 'is a palindrome' do
+      expect(racecar).to be_a_palindrome
+    end
+
+    # remove the 'x' before running this test
+    it 'is a palindrome' do
+      expect(rotator).to be_a_palindrome
+    end
+  end
+
+  context 'when a palindrome is not used' do
+    # remove the 'x' before running this test
+    it 'is not a palindrome' do
+      expect(spaceship).not_to be_a_palindrome
+    end
+
+    # remove the 'x' before running this test
+    it 'is not a palindrome' do
+      expect(palindrome).not_to be_a_palindrome
+    end
+  end
+end
